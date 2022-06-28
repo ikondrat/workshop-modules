b2fd173b07ff8c1ec02315d3ca05b68163f1d69b
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2022-06-28 23:01:09 +0200

    rm unused

--- packages/app-busket/jest.config.js
+++
@@ -1,6 +0,0 @@
-const { createJestConfig } = require('@craco/craco')
-
-const cracoConfig = require('./craco.config.js')
-const jestConfig = createJestConfig(cracoConfig({ env: process.env.NODE_ENV }))
-
-module.exports = jestConfig
